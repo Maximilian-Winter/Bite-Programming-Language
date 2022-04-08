e387603a12d91606db82c802821b80914bb19acf
Maximilian Winter <maximilian.winter.91@gmail.com>
2022-04-08 04:28:11 +0200

    Reordered folder and files.

--- Bite/Runtime/Memory/DynamicBiteVariable.cs
+++ Bite/Runtime/Memory/DynamicBiteVariable.cs
@@ -4,16 +4,6 @@ using System.Runtime.InteropServices;
 namespace Bite.Runtime.Memory
 {
 
-public enum DynamicVariableType : uint
-{
-    Null = 0xfff80001, // Use NaN boxing
-    True = 0xfff80002,
-    False = 0xfff80003,
-    String = 0xfff80004,
-    Array = 0xfff80005,
-    Object = 0xfff80006
-}
-
 [StructLayout( LayoutKind.Explicit )]
 public class DynamicBiteVariable
 {
---
+++ Bite/Runtime/Memory/DynamicVariableType.cs
@@ -0,0 +1,14 @@
+﻿namespace Bite.Runtime.Memory
+{
+
+public enum DynamicVariableType : uint
+{
+    Null = 0xfff80001, // Use NaN boxing
+    True = 0xfff80002,
+    False = 0xfff80003,
+    String = 0xfff80004,
+    Array = 0xfff80005,
+    Object = 0xfff80006
+}
+
+}
