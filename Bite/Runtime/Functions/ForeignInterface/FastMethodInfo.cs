765b69b016fb42c5b876867aac18161f663e2593
Maximilian Winter <maximilian.winter.91@gmail.com>
2022-04-16 03:50:59 +0200

    Update FastMethodInfo.cs

--- Bite/Runtime/Functions/ForeignInterface/FastMethodInfo.cs
+++ Bite/Runtime/Functions/ForeignInterface/FastMethodInfo.cs
@@ -70,4 +70,125 @@ public class FastMethodInfo
     #endregion
 }
 
+public class FastPropertyInfo < T >
+{
+    private delegate object ReturnValueDelegate( T instance, object[] arguments );
+
+    private delegate void UpdateValueDelegate( T instance, object value, object[] arguments );
+
+    private ReturnValueDelegate GetterDelegate { get; }
+
+    private UpdateValueDelegate SetterDelegate { get; }
+
+    #region Public
+
+    public FastPropertyInfo( PropertyInfo propertyInfo )
+    {
+        ParameterExpression instanceExpression = Expression.Parameter( typeof( T ), "instance" );
+        ParameterExpression valueExpression = Expression.Parameter( typeof( object ), "value" );
+        ParameterExpression argumentsExpression = Expression.Parameter( typeof( object[] ), "arguments" );
+        List < Expression > argumentExpressions = new List < Expression >();
+        ParameterInfo[] parameterInfos = propertyInfo.GetIndexParameters();
+
+        for ( int i = 0; i < parameterInfos.Length; ++i )
+        {
+            ParameterInfo parameterInfo = parameterInfos[i];
+
+            argumentExpressions.Add(
+                Expression.Convert(
+                    Expression.ArrayIndex( argumentsExpression, Expression.Constant( i ) ),
+                    parameterInfo.ParameterType ) );
+        }
+
+        IndexExpression callExpression = Expression.Property(
+            instanceExpression,
+            propertyInfo,
+            argumentExpressions );
+
+        BinaryExpression assignExpression = Expression.Assign(
+            callExpression,
+            Expression.Convert( valueExpression, propertyInfo.PropertyType ) );
+
+        GetterDelegate = Expression.Lambda < ReturnValueDelegate >(
+                                        Expression.Convert( callExpression, typeof( object ) ),
+                                        instanceExpression,
+                                        argumentsExpression ).
+                                    Compile();
+
+        SetterDelegate = Expression.Lambda < UpdateValueDelegate >(
+                                        assignExpression,
+                                        instanceExpression,
+                                        valueExpression,
+                                        argumentsExpression ).
+                                    Compile();
+    }
+
+    public object InvokeGet( object instance, params object[] arguments )
+    {
+        return GetterDelegate( ( T ) instance, arguments );
+    }
+
+    public void InvokeSet( object instance, object value, params object[] arguments )
+    {
+        SetterDelegate( ( T ) instance, value, arguments );
+    }
+
+    #endregion
+}
+
+public class FastFieldInfo
+{
+    private delegate object ReturnValueDelegate( object instance );
+
+    private delegate void UpdateValueDelegate( object instance, object value );
+    
+    private UpdateValueDelegate SetDelegate { get; }
+    
+    private ReturnValueDelegate Delegate { get; }
+
+    #region Public
+
+    public FastFieldInfo( FieldInfo propertyInfo )
+    {
+        ParameterExpression instanceExpression = Expression.Parameter( typeof( object ), "instance" );
+        ParameterExpression valueExpression = Expression.Parameter( typeof( object ), "value" );
+        MemberExpression callExpression = Expression.Field(
+            !propertyInfo.IsStatic ? Expression.Convert( instanceExpression, propertyInfo.ReflectedType ) : null,
+            propertyInfo );
+
+        Delegate = Expression.Lambda < ReturnValueDelegate >(
+                                  Expression.Convert( callExpression, typeof( object ) ),
+                                  instanceExpression ).
+                              Compile();
+        
+        BinaryExpression assignExpression = Expression.Assign(
+            callExpression,
+            Expression.Convert( valueExpression, propertyInfo.FieldType ) );
+        
+        SetDelegate =  Expression.Lambda < UpdateValueDelegate >(
+                                      assignExpression,
+                                      instanceExpression,
+                                      valueExpression).
+                                  Compile();
+    }
+
+    public object GetField( object instance )
+    {
+        return Delegate( instance );
+    }
+
+    public void SetField( object instance, object value )
+    {
+        SetDelegate( instance, value );
+    }
+    
+    #endregion
+}
+
+    public interface IFastPropertyInfo
+    {
+        object InvokeGet(object instance, params object[] arguments);
+        void InvokeSet(object instance, object value, params object[] arguments);
+    }
+
 }
